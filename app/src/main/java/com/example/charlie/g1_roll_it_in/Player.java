0d20e8a03e31f90246aa7f081a716cb62db02a0a
charlie0605 <charlie0605@hotmail.com>
2017-04-04 10:48:22 +1200

    Player class added

---
+++ app/src/main/java/com/example/charlie/g1_roll_it_in/Player.java
@@ -0,0 +1,16 @@
+package com.example.charlie.g1_roll_it_in;
+
+/**
+ * Created by Charlie on 4/04/2017.
+ */
+
+public class Player {
+
+    private String name;
+    private int score;
+
+    public Player(String name){
+        this.name = name;
+        this.score = 0;
+    }
+}
