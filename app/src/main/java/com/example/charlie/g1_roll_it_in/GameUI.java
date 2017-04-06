9fa8e93ff1ad10c69d9aee380f43a2c2060d947e
charlie0605 <charlie0605@hotmail.com>
2017-04-07 10:51:33 +1200

    update

--- app/src/main/java/com/example/charlie/g1_roll_it_in/GameUI.java
+++ app/src/main/java/com/example/charlie/g1_roll_it_in/GameUI.java
@@ -2,13 +2,15 @@ package com.example.charlie.g1_roll_it_in;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class GameUI extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game_ui);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.menu);
         //hey, this is me!
         //hello, this is a test from Thong.
         //another test string
