218492d5db0ce7bf0ab59193a0beef711d83d6d4
thongteav <thong.teav@gmail.com>
2017-04-09 13:28:37 +1200

    Added a goal. Added functionality to check for goal and make ball disappear.

--- app/src/main/java/com/example/charlie/g1_roll_it_in/Ball.java
+++ app/src/main/java/com/example/charlie/g1_roll_it_in/Ball.java
@@ -1,17 +1,32 @@
 package com.example.charlie.g1_roll_it_in;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.shapes.OvalShape;
+
 /**
  * Created by Thong on 5/04/2017.
  */
 
 public class Ball extends GameObject{
+    private int color;
+    private float radius;
 
-    public Ball(int x, int y){
-        super(x, y);
+    public Ball(int x, int y, float radius, int screenWidth, int screenHeight){
+        super(x, y, screenWidth, screenHeight);
+        this.radius = radius;
+        this.color = Color.RED;
+    }
+
+    public int getColor(){
+        return Color.argb(255, 255, 255, 255);
+        //will make this random
     }
 
     public void moveX(){
         this.x += speedX;
+        //might do checking here before adding speed to lessen the lagging effect
     }
 
     public void moveY(){
@@ -22,10 +37,48 @@ public class Ball extends GameObject{
     public void update() {
         this.moveX();
         this.moveY();
+        if(x <= radius || x >= screenWidth - radius){
+            speedX *= -1;
+        }
+        if(y <= radius || y >= screenHeight - radius){
+            speedY *= -1;
+        }
     }
 
     @Override
-    public void render() {
+    public void draw(Canvas canvas) {
+        Paint paint = new Paint();
+        paint.setColor(this.color);
+        canvas.drawCircle(this.x, this.y, this.radius, paint);
+    }
 
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    @Override
+    public void setSpeedX(float speedX){
+        if(Math.abs(speedX) > 10000){
+            this.speedX = speedX / 100;
+        } else if(Math.abs(speedX) > 1000) {
+            this.speedX = speedX / 10;
+        } else {
+            this.speedX = speedX;
+        }
+    }
+
+    @Override
+    public void setSpeedY(float speedY){
+        if(Math.abs(speedY) > 10000){
+            this.speedY = speedY / 1000;
+        } else if(Math.abs(speedY) > 1000) {
+            this.speedY = speedY / 100;
+        } else {
+            this.speedY = speedY;
+        }
+    }
+
+    public float getRadius() {
+        return radius;
     }
 }
--- app/src/main/java/com/example/charlie/g1_roll_it_in/GameUI.java
+++ app/src/main/java/com/example/charlie/g1_roll_it_in/GameUI.java
@@ -1,21 +1,24 @@
 package com.example.charlie.g1_roll_it_in;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Window;
 import android.view.WindowManager;
 
-public class GameUI extends AppCompatActivity {
+public class GameUI extends Activity {
+    public static int screenWidth, screenHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        DisplayMetrics metrics = new DisplayMetrics();
+        this.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        int screenHeight = metrics.heightPixels;
+        int screenWidth = metrics.widthPixels;
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(new MyView(this));
-//        setContentView(R.layout.menu);
-        //hey, this is me!
-        //hello, this is a test from Thong.
-        //another test string
-        //Charlie, Testing
-        //Thong, from charlie branch
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(new GameView(this));
     }
 }
