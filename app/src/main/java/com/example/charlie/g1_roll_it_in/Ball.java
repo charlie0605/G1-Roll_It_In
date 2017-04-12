0595c04ffe4442a2782e025b6b180ebe169ea47a
thongteav <thong.teav@gmail.com>
2017-04-12 14:50:14 +1200

    Added color changing to the ball

--- app/src/main/java/com/example/charlie/g1_roll_it_in/Ball.java
+++ app/src/main/java/com/example/charlie/g1_roll_it_in/Ball.java
@@ -2,6 +2,9 @@ package com.example.charlie.g1_roll_it_in;
 
 import android.graphics.Canvas;
 import android.graphics.Color;
+
+import java.util.Random;
+
 /**
  * Created by Thong on 5/04/2017.
  */
@@ -10,12 +13,15 @@ public class Ball extends RoundObject{
 
     public Ball(int x, int y, float radius){
         super(x, y, radius);
-        this.color = Color.RED;//get a random color for each ball
+        this.color = getRandomColor();//get a random color for each ball
     }
 
     public int getRandomColor(){
-        return Color.argb(255, 255, 0, 0);
-        //will make this random
+        Random rand = new Random();
+        int red = rand.nextInt(256);
+        int green = rand.nextInt(256);
+        int blue = rand.nextInt(256);
+        return Color.argb(255, red, green, blue);
     }
 
     @Override
@@ -32,14 +38,14 @@ public class Ball extends RoundObject{
                 x = (int) (GameView.width - radius);
             }
         }
-        
-        if(y <= radius || x >= GameView.height - radius){
-            speedX *= -1;
+
+        if(y <= radius || y >= GameView.height - radius){
+            speedY *= -1;
             if(y < radius) {
                 y = (int) radius;
             }
-            if(x > GameView.height - radius){
-                x = (int) (GameView.height - radius);
+            if(y > GameView.height - radius){
+                y = (int) (GameView.height - radius);
             }
         }
     }
