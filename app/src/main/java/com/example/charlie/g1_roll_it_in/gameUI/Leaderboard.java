5dc170b446ba46f162539bb75a1e454b38219669
raidenzhang <cloudzhang7@gmail.com>
2017-05-15 23:53:31 +1200

    leaderboard

---
+++ app/src/main/java/com/example/charlie/g1_roll_it_in/gameUI/Leaderboard.java
@@ -0,0 +1,12 @@
+package com.example.charlie.g1_roll_it_in.gameUI;
+
+import android.app.Activity;
+
+
+/**
+ * Created by Raiden on 2017/5/13 0015.
+ */
+
+public class Leaderboard extends Activity{
+
+}
